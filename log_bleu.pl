e0874d7d11551011813c9a120243dbf334cc509a
DCSaunders <ds636@cam.ac.uk>
2017-07-11 12:58:54 +0100

    add step number, include single-digit bleu

--- log_bleu.pl
+++ log_bleu.pl
@@ -5,13 +5,23 @@ my $f_name = $ARGV[0];
 open(my $in, "<", $f_name) 
     or die "$f_name not openable";
 my ($bleu, $ug, $bg, $tg, $fg, $bp);
-
+my $last_step;
+my $tmp_step;
+my $best_bleu=0;
 while (<$in>)
 {
     chomp;
     my $line = $_;
-    ($bleu, $ug, $bg, $tg, $fg, $bp) = ($line =~ /BLEU\s=\s(\d\d\.\d\d),\s(\d\d\.\d)\/(\d\d\.\d)\/(\d\d\.\d)\/(\d\d\.\d)\s\(BP=(\d\.\d\d\d)/);
+    ($tmp_step) = ($line =~ /Global\sstep=(\d+)/);
+    if (length $tmp_step){
+	$last_step = $tmp_step;
+    }
+    ($bleu, $ug, $bg, $tg, $fg, $bp) = ($line =~ /BLEU\s=\s(\d+\.\d\d),\s(\d+\.\d)\/(\d+\.\d)\/(\d+\.\d)\/(\d+\.\d)\s\(BP=(\d\.\d\d\d)/);
     if (length $bleu){
-	print "$bleu, $ug, $bg, $tg, $fg, $bp\n";
+	if ($bleu > $best_bleu) {
+	    print "newbest_";
+	    $best_bleu = $bleu;
+	}
+	print "step_$last_step  $bleu, $ug, $bg, $tg, $fg, $bp\n";
     }
 }
